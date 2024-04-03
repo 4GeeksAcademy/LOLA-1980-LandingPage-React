4ec44c1b87b9b1f7bc4a986254b94a668ec0d52c
LOLA-1980 <109870340+LOLA-1980@users.noreply.github.com>
2024-04-03 20:25:39 +0000

    [Landing Page con React, se le agregaron estilos y contenido al componente Card, se le agrego el nombre de Start Bootstrap al Navbar y se hicieron unos ajustes enter el Home]

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 // Definición del componente Card
 const Card = (props) => {
   return (
-    <div className="card mt-4 col-md-3 me-4" style={{ width: '18rem' }}>
+    <div className="card mt-4 col-md-3 me-1" style={{ width: '20rem' }}>
         <img src={props.image} className="card-img-top img-cover" alt="..."  />
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.description}</p>
-            <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
+        </div>
+        <div className="d-flex justify-content-center mb-3">
+          <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
         </div>
     </div>
-
   );
 }
 
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
